a86f7e00b7c73a7b0a717fd3ae6e2a2849a30856
cubebuilder <sebastiantmk@gmail.com>
2020-08-06 12:37:57 -0400

    Update conn.php

--- spacemy.xyz/func/conn.php
+++ spacemy.xyz/func/conn.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect("localhost", "cubebuilder", "cubebuilder5b5t$", "spacemy");
-    define("CAPTCHA_PRIVATEKEY", "6LcgYrsZAAAAAC9pf7R0RbiBiosnyPMmSqEgzNtA]");
-	define("CAPTCHA_SITEKEY", "6LcgYrsZAAAAACSsY3uD_Y2S6VWZlo8rl35tG1tI");
+    $conn = mysqli_connect("localhost", "root", "dbpassword", "spacemy");
+    define("CAPTCHA_PRIVATEKEY", "");
+	define("CAPTCHA_SITEKEY", "");
